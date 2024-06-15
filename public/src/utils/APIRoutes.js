3fed138bc72459b047d68e058bce867bcb7be7c3
JAMPANA MAHEETH SAI DURGA VARMA <121691092+Mahithvarma@users.noreply.github.com>
2024-06-15 11:40:46 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://chat-app-server-iota-three.vercel.app";
+export const host = "https://chat-app-six-ashy.vercel.app";
 export const RegisterRoute = `${host}/api/auth/register`;
 export const LoginRoute = `${host}/api/auth/login`;
 export const allUsersRoute = `${host}/api/auth/allusers`;
