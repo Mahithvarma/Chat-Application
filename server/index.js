ac441a910fcb3c69fe51aee63d5479cac619b41b
JAMPANA MAHEETH SAI DURGA VARMA <121691092+Mahithvarma@users.noreply.github.com>
2024-06-18 17:36:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ const server = app.listen(port, () => {
 
 const io = socket(server, {
     cors: {
-        origin: "*",
+        origin: "https://chat-app-mahith.netlify.app",
         credentials: true,
     },
 });
